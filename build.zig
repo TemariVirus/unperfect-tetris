0fa62034dacbe4d2c95020fef44766da2d84bf6a
Zemogus <kiyonnxperiaz@gmail.com>
2024-06-06 03:21:57 +0800

    Add bots

--- build.zig
+++ build.zig
@@ -20,8 +20,54 @@ pub fn build(b: *std.Build) void {
     }).module("engine");
     exe.root_module.addImport("engine", engine_module);
 
+    // Add NN files
+    const install_NNs = b.addInstallDirectory(.{
+        .source_dir = .{ .path = "NNs" },
+        .install_dir = .bin,
+        .install_subdir = "NNs",
+    });
+    exe.step.dependOn(&install_NNs.step);
+
+    // Add run step
     const run_cmd = b.addRunArtifact(exe);
     run_cmd.step.dependOn(b.getInstallStep());
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
+
+    // Add test step
+    buildTests(b, engine_module);
+
+    // Add bench step
+    buildBench(b, target, engine_module);
+}
+
+fn buildTests(b: *std.Build, engine_module: *std.Build.Module) void {
+    const lib_tests = b.addTest(.{
+        .root_source_file = .{ .path = "src/root.zig" },
+    });
+    lib_tests.root_module.addImport("engine", engine_module);
+    const run_lib_tests = b.addRunArtifact(lib_tests);
+
+    const test_step = b.step("test", "Run library tests");
+    test_step.dependOn(&run_lib_tests.step);
+}
+
+fn buildBench(b: *std.Build, target: std.Build.ResolvedTarget, engine_module: *std.Build.Module) void {
+    const bench_exe = b.addExecutable(.{
+        .name = "Budget Tetris Bot Benchmarks",
+        .root_source_file = .{ .path = "src/bench.zig" },
+        .target = target,
+        .optimize = .ReleaseFast,
+    });
+    bench_exe.root_module.addImport("engine", engine_module);
+
+    b.installArtifact(bench_exe);
+
+    const bench_cmd = b.addRunArtifact(bench_exe);
+    bench_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        bench_cmd.addArgs(args);
+    }
+    const bench_step = b.step("bench", "Run benchmarks");
+    bench_step.dependOn(&bench_cmd.step);
 }
