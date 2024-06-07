0ad8600be2eaab9940877ecbbef433dedf642a41
Zemogus <kiyonnxperiaz@gmail.com>
2024-06-07 14:47:27 +0800

    Add PC demo

--- build.zig
+++ build.zig
@@ -1,6 +1,7 @@
 const std = @import("std");
+const Build = std.Build;
 
-pub fn build(b: *std.Build) void {
+pub fn build(b: *Build) void {
     const target = b.standardTargetOptions(.{});
     const optimize = b.standardOptimizeOption(.{});
 
@@ -20,6 +21,8 @@ pub fn build(b: *std.Build) void {
     }).module("engine");
     exe.root_module.addImport("engine", engine_module);
 
+    const nterm_module = engine_module.import_table.get("nterm").?;
+
     // Add NN files
     const install_NNs = b.addInstallDirectory(.{
         .source_dir = .{ .path = "NNs" },
@@ -34,14 +37,42 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    // Add test step
+    buildDemo(b, target, optimize, engine_module, nterm_module, install_NNs);
     buildTests(b, engine_module);
-
-    // Add bench step
     buildBench(b, target, engine_module);
 }
 
-fn buildTests(b: *std.Build, engine_module: *std.Build.Module) void {
+fn buildDemo(
+    b: *Build,
+    target: Build.ResolvedTarget,
+    optimize: std.builtin.OptimizeMode,
+    engine_module: *Build.Module,
+    nterm_module: *Build.Module,
+    install_NNs: *Build.Step.InstallDir,
+) void {
+    const train_exe = b.addExecutable(.{
+        .name = "perfect-tetris-demo",
+        .root_source_file = .{ .path = "src/demo.zig" },
+        .target = target,
+        .optimize = optimize,
+    });
+    train_exe.root_module.addImport("engine", engine_module);
+    train_exe.root_module.addImport("nterm", nterm_module);
+
+    b.installArtifact(train_exe);
+
+    // Add demo step
+    const run_cmd = b.addRunArtifact(train_exe);
+    run_cmd.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        run_cmd.addArgs(args);
+    }
+    const run_step = b.step("demo", "Run the demo");
+    train_exe.step.dependOn(&install_NNs.step);
+    run_step.dependOn(&run_cmd.step);
+}
+
+fn buildTests(b: *Build, engine_module: *Build.Module) void {
     const lib_tests = b.addTest(.{
         .root_source_file = .{ .path = "src/root.zig" },
     });
@@ -52,7 +83,7 @@ fn buildTests(b: *std.Build, engine_module: *std.Build.Module) void {
     test_step.dependOn(&run_lib_tests.step);
 }
 
-fn buildBench(b: *std.Build, target: std.Build.ResolvedTarget, engine_module: *std.Build.Module) void {
+fn buildBench(b: *Build, target: Build.ResolvedTarget, engine_module: *Build.Module) void {
     const bench_exe = b.addExecutable(.{
         .name = "Budget Tetris Bot Benchmarks",
         .root_source_file = .{ .path = "src/bench.zig" },
---
+++ src/demo.zig
@@ -0,0 +1,135 @@
+const std = @import("std");
+const Allocator = std.mem.Allocator;
+const time = std.time;
+
+const engine = @import("engine");
+const GameState = Player.GameState;
+const kicks = engine.kicks;
+const PeriodicTrigger = engine.PeriodicTrigger;
+const Player = engine.Player(SevenBag);
+const SevenBag = engine.bags.SevenBag;
+
+const nterm = @import("nterm");
+const View = nterm.View;
+
+const root = @import("root.zig");
+const NN = root.neat.NN;
+const pc = root.pc;
+const Placement = root.Placement;
+
+const FRAMERATE = 10;
+const FPS_TIMING_WINDOW = FRAMERATE * 2;
+const MAX_PC_QUEUE = 16;
+
+pub fn main() !void {
+    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
+    const allocator = gpa.allocator();
+    defer _ = gpa.deinit();
+
+    // Add 2 to create a 1-wide empty boarder on the left and right.
+    try nterm.init(allocator, std.io.getStdOut(), FPS_TIMING_WINDOW, Player.DISPLAY_W + 2, Player.DISPLAY_H);
+    defer nterm.deinit();
+
+    const settings = engine.GameSettings{
+        .g = 0,
+        .target_mode = .none,
+    };
+    const player_view = View{
+        .left = 1,
+        .top = 0,
+        .width = Player.DISPLAY_W,
+        .height = Player.DISPLAY_H,
+    };
+    var player = Player.init(
+        "You",
+        SevenBag.init(0),
+        kicks.srsPlus,
+        settings,
+        player_view,
+        playSfxDummy,
+    );
+
+    var placement_i: usize = 0;
+    var pc_queue = std.ArrayList([]Placement).init(allocator);
+    defer pc_queue.deinit();
+
+    const pc_thread = try std.Thread.spawn(.{
+        .allocator = allocator,
+    }, pcThread, .{ allocator, player.state, &pc_queue });
+    defer pc_thread.join();
+
+    const fps_view = View{
+        .left = 1,
+        .top = 0,
+        .width = 15,
+        .height = 1,
+    };
+
+    var render_timer = PeriodicTrigger.init(time.ns_per_s / FRAMERATE);
+    while (true) {
+        if (render_timer.trigger()) |dt| {
+            fps_view.printAt(0, 0, .white, .black, "{d:.2}FPS", .{nterm.fps()});
+
+            placePcPiece(allocator, &player, &pc_queue, &placement_i);
+            player.tick(dt, 0, &.{});
+            try player.draw();
+            nterm.render() catch |err| {
+                if (err == error.NotInitialized) {
+                    return;
+                }
+                return err;
+            };
+        } else {
+            time.sleep(1 * time.ns_per_ms);
+        }
+    }
+}
+
+fn placePcPiece(allocator: Allocator, game: *Player, queue: *std.ArrayList([]Placement), placement_i: *usize) void {
+    if (queue.items.len == 0) {
+        return;
+    }
+    const placements = queue.items[0];
+
+    const placement = placements[placement_i.*];
+    if (placement.piece.kind != game.state.current.kind) {
+        game.hold();
+    }
+    game.state.pos = placement.pos;
+    game.state.current = placement.piece;
+    game.hardDrop(0, &.{});
+    placement_i.* += 1;
+
+    if (placement_i.* == placements.len) {
+        allocator.free(queue.orderedRemove(0));
+        placement_i.* = 0;
+    }
+}
+
+fn pcThread(allocator: Allocator, state: GameState, queue: *std.ArrayList([]Placement)) !void {
+    var game = state;
+
+    const nn = try NN.load(allocator, "NNs/Fapae.json");
+    defer nn.deinit(allocator);
+
+    while (true) {
+        while (queue.items.len >= MAX_PC_QUEUE) {
+            time.sleep(time.ns_per_ms);
+        }
+
+        const placements = try pc.findPc(allocator, game, nn, 0, 16);
+        for (placements) |placement| {
+            if (game.current.kind != placement.piece.kind) {
+                game.hold();
+            }
+            game.current = placement.piece;
+            game.pos = placement.pos;
+            _ = game.lockCurrent(-1);
+            game.nextPiece();
+        }
+
+        try queue.append(placements);
+    }
+}
+
+fn playSfxDummy(_: engine.player.Sfx) void {}
